d92a52020938dafe9921d0aadb94fbb74ad9dd48
Maria Kozlovtseva <mashak1407@gmail.com.com>
2020-10-23 19:32:48 +0300

    1. Do arguments handling 2. Add a question to the user about the wish of the request 3. Add README.md

---
+++ README.md
@@ -0,0 +1,51 @@
+###Text index
+This program works with files in .txt format, composes an index of the text file and responds to user requests.
+
+**Description of correct format of input data:**
+* one file in .txt format
+* the file contains coherent text in Russian
+
+####Program operation mode
+* Compiling a text index for a file that does not already have one.
+    >This file does not yet have a text index. The index will be compiled.  
+    ...  
+    Index is compiled. Do you want to make a request? Enter "yes" or "no".
+
+    >This file already has a text index.  
+    Do you want to make a request? Enter "yes" or "no".
+   
+* Displays a response to a user request for a file that already has a text index.
+    >Please select the type of request you want 
+    (enter the number and input data of this type of request separated by space):...                                                                                                                                                                                                                                                                                                                
+
+####Types of requests:
+1. Get a list of the given number of the most common words.  
+* **Input data:** one natural number
+* **Output data:**
+    
+2. Get full information about the use of a given word 
+    (number of occurrences, used word forms, page numbers).
+* **Input data:** one word in Russian
+* **Output data:**
+        
+3. Get full information (see item 2) about the use of words from a given group 
+    (for example, furniture items, verbs of movement, etc.).
+* **Input data:** nothing
+* A list of existing groups will be displayed.
+  Select one of the suggested groups (copy the line you want and paste into the input line) 
+  or create a new one:
+    - 
+* **Output data:**
+        
+4. Output all lines containing a given word (in any of the word forms).
+* **Input data:** one word in Russian
+* **Output data:**
+    
+    
+
+
+####Example
+````bash
+
+````
+
--- src/main/TextIndex.kt
+++ src/main/TextIndex.kt
@@ -1,1 +1,138 @@
 package program
+
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+/**
+ * This program works with files in .txt format, composes an index of the text file and responds to user requests.
+ * @param {String} filename (file in .txt format)
+ */
+
+/**
+ * This class indicates that an incorrect number of files are filed or in an incorrect format.
+ */
+class IncorrectArgsFormat(message: String): Exception(message)
+
+/**
+ * This class indicates that it is incorrect answer.
+ * The user is informed about the rules for writing a answer.
+ */
+class IncorrectAnswerFormat(message: String): Exception(message)
+
+/**
+ * This function generates a log file with the exact time in the name in the folder "logs".
+ * @return log file
+ * The log file will contain explanations of the type of error.
+ */
+fun createLogFile(): File {
+    val logsDir = File("logs/")
+    logsDir.mkdir()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val dateTime = LocalDateTime.now().format(formatter)
+    val logName = "$dateTime.log"
+    val logFile = File(logsDir, logName)
+    return logFile
+}
+
+/**
+ * This function checks that user has given only one filename.
+ */
+fun isOneFile(args: Array<String>): String? {
+    when (args.size) {
+        0 -> IncorrectArgsFormat("No files are listed! Expect one file in .txt format.")
+        1 -> { return args[0] }
+        else -> IncorrectArgsFormat("Too many arguments! Expect one file in .txt format.")
+    }
+    return null
+}
+
+/**
+ * This function checks that [filename] in .txt format.
+ */
+fun isTXTFile(filename: String) {
+    if (filename.takeLast(4) != ".txt") {
+        IncorrectArgsFormat("Incorrect file format! Expect one file in .txt format.")
+    }
+}
+
+/**
+ * This function shows how file with text index for [filename] should look.
+ * @return path to index file
+ */
+fun nameOfIndexFile(filename: String): File {
+    val indexDir = File("indices/")
+    indexDir.mkdir()
+    val indexFileName = "index_$filename"
+    val indexFile = File(indexDir, indexFileName)
+    return indexFile
+}
+
+/**
+ * This function checks if [filename] has file with text index.
+ */
+fun haveIndexFile(filename: String): Boolean {
+    return nameOfIndexFile(filename).exists()
+}
+
+/**
+ * This function creates an index of the text for [filename], if it does not already exist,
+ * and asks if the user wants to make a request.
+ */
+fun doYouWantMakeRequest(filename: String) {
+    if (haveIndexFile(filename)) {
+        println("This file does not yet have a text index. The index will be compiled.")
+        createIndexFile(filename)
+        println("Index is compiled. Do you want to make a request? Enter \"yes\" or \"no\".")
+    } else
+        println(
+            """This file already has a text index.
+               Do you want to make a request? Enter "yes" or "no"."""
+        )
+}
+
+/**
+ * This function processes the response to the question if the user wants to make a request.
+ */
+fun processingFirstAnswer(): Boolean? {
+    val wantToMakeRequest = readLine()
+    when (wantToMakeRequest) {
+        "yes" -> return true
+        "no"  -> return false
+        else  -> {
+            IncorrectAnswerFormat("Incorrect answer format! Enter \"yes\" or \"no\"")
+            processingFirstAnswer()
+        }
+    }
+    return null
+}
+
+fun createIndexFile(filename: String) {
+    val indexFile = nameOfIndexFile(filename)
+    //дохрена всего
+}
+
+
+
+//индекс должен где-то храниться
+//пункт с группами: есть уже какой-то список, который пользователь может пополнять
+//файлик не изменится (если ловим исключение, говорим, что надо перестроить индекс)
+
+fun main(args: Array<String>) {
+    val logFile = createLogFile()
+
+    try {
+        val filename = isOneFile(args)
+        isTXTFile(filename!!)// проверить, что файл состоит из русского связного текста
+        doYouWantMakeRequest(filename)
+
+
+    }
+    catch (e: Exception) {
+        /**
+         * This block prints an error message to the screen and writes a description of the error to log.
+         */
+        logFile.appendText("$e\n")
+    }
+}
